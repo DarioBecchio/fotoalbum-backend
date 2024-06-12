b0107809e6bd3e2232c032c35ff0ef23032527e1
Dario Becchio <dabek1986@gmail.com>
2024-06-12 22:01:17 +0200

    create image/store

---
+++ resources/views/partials/errors.blade.php
@@ -0,0 +1,11 @@
+@if($errors->any())
+        <div
+            class="alert alert-danger"
+            role="alert"
+        >
+            @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </div>
+        
+@endif
