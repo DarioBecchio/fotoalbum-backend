998993b5dedeff866e3836e99d5e83030b7e033b
Dario Becchio <dabek1986@gmail.com>
2024-06-13 17:41:31 +0200

    Create Category table, create reelations one to many with Photo model

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 
 class Photo extends Model
 {
     use HasFactory;
     protected $fillable = ['title','description','image_path'];
+
+    public function type (): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
